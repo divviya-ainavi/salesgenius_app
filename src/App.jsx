68cd2c3bc10bc098c2a4b333be9ffeb31a4a65b6
Divviya <85920511+DivviyaC@users.noreply.github.com>
2025-06-26 21:27:48 +0530

    Revert "update posthog identity"
    This reverts commit a6601abdd83afacbdc1c44a099c8e760ee0c9ab3.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { CURRENT_USER } from "./lib/supabase";
-import posthog from "./lib/posthogclient";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    // Identify user once on load
-    posthog.identify(CURRENT_USER?.email, {
-      email: CURRENT_USER?.email,
-    });
-  }, []);
   return (
     <>
       <div>
@@ -37,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/lib/posthogclient.js
+++
@@ -1,9 +0,0 @@
-// src/posthogClient.js
-import posthog from 'posthog-js'
-
-posthog.init('phc_OwCAqcaP8ahG4awoddGdb3FvhWkZ5gM4wFBSbbtFe65', {
-    api_host: 'https://eu.i.posthog.com', // or your self-hosted domain
-    capture_pageview: true,
-})
-
-export default posthog
