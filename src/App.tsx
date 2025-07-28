bddd537bda6a2223200656c42db941c07ef3c3e7
AINaviHaja <haja@gpsglobal.co.uk>
2025-07-28 17:09:04 +0530

    Add missing forgot password and reset password routes

--- src/App.tsx
+++ src/App.tsx
@@ -64,6 +64,8 @@ const App = () => {
           <Routes>
             {/* Public Routes */}
             <Route path="/auth/login" element={<LoginPage />} />
+            <Route path="/auth/forgot-password" element={<ForgotPassword />} />
+            <Route path="/auth/reset-password" element={<ResetPassword />} />
             <Route path="/auth/setup" element={<AccountSetup />} />
             <Route path="/hubspot-callback" element={<HubSpotCallback />} />
 
