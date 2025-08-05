04ee6acf437ef699dcc3e0a9811a17e908d2d73a
AINaviHaja <haja@gpsglobal.co.uk>
2025-08-05 13:29:35 +0530

    Updated OnboardingTour.jsx

--- src/components/onboarding/OnboardingTour.jsx
+++ src/components/onboarding/OnboardingTour.jsx
@@ -1,35 +1,34 @@
-@@ .. @@
- import { useSelector, useDispatch } from 'react-redux';
- import { dbHelpers } from '@/lib/supabase';
- import { analytics } from '@/lib/analytics';
--import { setUser } from '@/store/slices/authSlice';
-+import { setHasSeenOnboardingTour } from '@/store/slices/authSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { dbHelpers } from '@/lib/supabase';
+import { analytics } from '@/lib/analytics';
+import { setHasSeenOnboardingTour } from '@/store/slices/authSlice';
 
- export const OnboardingTour = () => {
-   const dispatch = useDispatch();
--  const { user } = useSelector((state) => state.auth);
-+  const { user, hasSeenOnboardingTour } = useSelector((state) => state.auth);
-   const location = useLocation();
-   
-   const [run, setRun] = useState(false);
-@@ .. @@
-   // Check if tour should run
-   useEffect(() => {
-     const shouldRunTour = () => {
--      if (!user || user.has_seen_onboarding_tour) {
-+      if (!user || hasSeenOnboardingTour) {
-         return false;
-       }
-@@ .. @@
-       try {
-         await dbHelpers.updateOnboardingTourStatus(user.id, true);
-         
--        // Update Redux state
--        dispatch(setUser({
--          ...user,
--          has_seen_onboarding_tour: true
--        }));
-+        // Update Redux state  
-+        dispatch(setHasSeenOnboardingTour(true));
-         
-         analytics.track('onboarding_tour_completed', {
+export const OnboardingTour = () => {
+  const dispatch = useDispatch();
+  const { user, hasSeenOnboardingTour } = useSelector((state) => state.auth);
+  const location = useLocation();
+  
+  const [run, setRun] = useState(false);
+  
+  // Check if tour should run
+  useEffect(() => {
+    const shouldRunTour = () => {
+      if (!user || hasSeenOnboardingTour) {
+        return false;
+      }
+    };
+  });
+  
+  const handleTourComplete = async () => {
+    try {
+      await dbHelpers.updateOnboardingTourStatus(user.id, true);
+      
+      // Update Redux state  
+      dispatch(setHasSeenOnboardingTour(true));
+      
+      analytics.track('onboarding_tour_completed', {
+      });
+    } catch (error) {
+    }
+  };
+};
