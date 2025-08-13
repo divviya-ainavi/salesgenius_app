c5dc20e2a153acbee6d3af8d589871f93c1d9e50
AINaviHaja <haja@gpsglobal.co.uk>
2025-08-13 11:20:44 +0530

    Updated Settings.tsx

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1057,7 +1057,7 @@ export const Settings = () => {
     }
   };
 
-  const handleDeleteBusinessMaterial = async (materialId: string) => {
+  const handleDeleteBusinessMaterial = async (materialId) => {
     try {
       // Update is_active to false in database
       await dbHelpers.updateInternalUploadedFileStatus(materialId, false);
@@ -1155,8 +1155,7 @@ export const Settings = () => {
             hubspot_user_details: ownersArray,
           }
         );
-          }
-        );
+
         // Update Redux state
         dispatch(
           setHubspotIntegration({
