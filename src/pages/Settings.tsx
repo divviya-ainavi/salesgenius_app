2d41eef38c0cd82a644e1920694ab5966dd9bc57
AINaviHaja <haja@gpsglobal.co.uk>
2025-09-01 13:47:43 +0530

    Add edit icon between View Files and Delete buttons

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -3479,6 +3479,14 @@ export const Settings = () => {
                               >
                                 <Trash2 className="w-4 h-4" />
                               </Button>
+                              <Button
+                                variant="outline"
+                                size="sm"
+                                onClick={() => handleViewBusinessKnowledge(item)}
+                                className="text-green-600 hover:bg-green-50"
+                              >
+                                <Edit className="w-4 h-4" />
+                              </Button>
                             </div>
                           </div>
                         ))} */}
