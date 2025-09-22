a6c266ad1bf2c038cfeefac190dacb7989190a53
Divviya <85920511+DivviyaC@users.noreply.github.com>
2025-09-22 14:12:14 +0530

    Revert "Updated Research.tsx"
    This reverts commit 2470186e0485bb326708158fac15a4530fc382ea.

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -11,6 +11,12 @@ import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
 import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
   Dialog,
   DialogContent,
   DialogDescription,
