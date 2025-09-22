d2bcbe4d4a544733a63f33a86acf6e5d220aa317
Divviya <85920511+DivviyaC@users.noreply.github.com>
2025-09-22 15:57:44 +0530

    Revert "Fix delete confirmation popup not opening"
    This reverts commit 901786f61418573c2bebc05b439192940e9a25bc.

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -12,20 +12,6 @@ import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import {
   Search,
   ThumbsUp,
   ThumbsDown,
